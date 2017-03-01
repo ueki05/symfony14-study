369c8599498e4b72743ad8f8d5296b2d4af5ef6f
ueki05 <uekiyuta1991@gmail.com>
2017-03-01 09:23:58 +0900

    add InquiryForm and sendmail

--- apps/frontend/modules/Inquiry/actions/actions.class.php
+++ apps/frontend/modules/Inquiry/actions/actions.class.php
@@ -1,9 +1,8 @@
 <?php
-
 /**
  * Inquiry actions.
  *
- * @package    sf14
+ * @package    symfonyband
  * @subpackage Inquiry
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
@@ -19,8 +18,25 @@ class InquiryActions extends sfActions
   {
     $this->forward('default', 'module');
   }
-
   public function executeNew(sfWebRequest $request)
   {
+    $form = new InquiryForm();
+    if ($request->isMethod(sfRequest::POST))
+    {
+      $form->bind($request->getParameter($form->getName()));
+      if ($form->isValid())
+      {
+        $form->send($this->context, 'yourname@example.com', 'Symfony楽団ホームページからの問い合わせ');
+        $this->getUser()->setAttribute('inquiry_send', true);
+        $this->redirect('Inquiry/Complete');
+      }
+    }
+    $this->form = $form;
   }
-}
+  public function executeComplete(sfWebRequest $request)
+  {
+    $user = $this->getUser();
+    $this->redirectUnless($user->getAttribute('inquiry_send'), 'Inquiry/New');
+    $user->setAttribute('inquiry_send', null);
+  }
+
